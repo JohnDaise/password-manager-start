aaa224782240c6930f0e856af23211e00cae692c
JohnDaise <jdaise@gmail.com>
2023-06-13 19:23:53 -0400

    setup UI witth lock logo

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from tkinter import *
+WHITE = "#FFFFFF"
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg=WHITE)
+canvas = Canvas(width=200, height=200, bg=WHITE, highlightthickness=5)
+
+lock_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_image)
+canvas.grid(column=1, row=1)
+
+window.mainloop()
